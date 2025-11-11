f5d0094edfcb4ed22b62b36b3f3b62f0c4a954e3
Sadiqur-Rahman <rahman2305341270@diu.edu.bd>
2025-11-11 19:54:27 +0600

    Django model for Employee

--- employee_service/api/models.py
+++ employee_service/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.charField(max_length=100)
+    department = models.charField(max_length=100)
+    role = models.charField(max_length=100)
+
+    def __str__(self):
+      return self.name
