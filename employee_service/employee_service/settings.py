97b022124fc58e3b325faff889bad910c44ba204
Sadiqur-Rahman <rahman2305341270@diu.edu.bd>
2025-11-11 19:30:57 +0600

    database connection created

--- employee_service/employee_service/settings.py
+++ employee_service/employee_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = "employee_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "emp_master",
+        'USER':'root',
+        'PASSWORD':"",
+        'HOST' : "localhost",
+        'PORT':3307,
     }
 }
 
